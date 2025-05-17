8f87f5730898e31a7082db4308c72dc54d6f4952
Valentin <197127997+demnep@users.noreply.github.com>
2025-05-17 14:18:33 +0200

    Added content to js file

--- ap.js
+++ ap.js
@@ -1,1 +1,10 @@
-alert('Hello World!');
+function getComputerChoice(max) {
+    if (Math.floor(Math.random() * max) === 0) {
+        return console.log('rock');
+    } else if (Math.floor(Math.random() * max) === 1) {
+        return console.log('papper');
+    } else {
+        return console.log('scissor');
+    }
+    console.log(getComputerChoice(3));
+}
