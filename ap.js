9ee1947f212a59903f7cb089368e45ef955f0961
Valentin <197127997+demnep@users.noreply.github.com>
2025-05-17 14:30:58 +0200

    Added human choices

--- ap.js
+++ ap.js
@@ -2,9 +2,21 @@ function getComputerChoice(max) {
     if (Math.floor(Math.random() * max) === 0) {
         return console.log('rock');
     } else if (Math.floor(Math.random() * max) === 1) {
-        return console.log('papper');
+        return console.log('paper');
     } else {
         return console.log('scissor');
     }
 }
     console.log(getComputerChoice(3));
+ function getHumanChoice(num) {
+    const choice = prompt('please enter rock, paper or scissor');
+    if (choice == 'rock') {
+        return console.log('human picked rock');
+    } else if (choice == 'paper') {
+        return console.log('human picked paper');
+    } else if (choice == 'scissor') {
+        return console.log('human picked scissor');
+    } else {
+        return console.log('human picked invalid');
+    }
+ }
